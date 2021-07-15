30fdddac0829f3198ae2160a2424263558b6b7dc
Vaibhav <vaibhavpandeyprayag@gmail.com>
2021-07-16 00:45:22 +0530

    First commit from VSCode
    Learning git commands

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <div className="bg-indigo-800 text-white text-xl p-4">
+        <h1>React Project Development</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
