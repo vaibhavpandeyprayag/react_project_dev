451a62c997dcff01c94fcbecf47c73b9ebf30e44
Vaibhav <vaibhavpandeyprayag@gmail.com>
2021-07-26 23:58:09 +0530

    Create Story snippet created, Existing stories recreated on the snippet

--- src/components/button/Button.stories.tsx
+++ src/components/button/Button.stories.tsx
@@ -1,14 +1,16 @@
 import Button from './Button';
 import "../../index.css";
 
-const Template = (args: any) => <Button {...args}></Button>
-export const main: any = Template.bind({});
-main.args = {
-  children: "Sign In",
-  type: "submit",
-  className: "",
-}
 export default {
   component: Button,
   title: 'Components/Button',
 }
+
+export const Main = (args: any) => <Button {...args}></Button>;
+
+Main.args = {
+  children: "Sign In",
+  type: "submit",
+  className: "",
+}
+
