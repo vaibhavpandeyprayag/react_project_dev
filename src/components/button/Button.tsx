33eea15026b4cd22d94693934ccd8be1e35995c9
Vaibhav <vaibhavpandeyprayag@gmail.com>
2021-07-26 22:56:23 +0530

    Button.stories.tsx created, tailwindCSS configured for storybook

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -9,11 +9,11 @@ const Button: FC<Props> = ({ theme, children, className, ...rest }) => {
   const themeClasses =
     theme === "primary"
       ? "bg-blue-600"
-      : "bg-gray-400";
+      : "bg-gray-500";
   return (
     <button
       {...rest}
-      className={themeClasses + " w-auto text-white text-sm py-2 px-5 shadow-xl filter drop-shadow-xl hover:shadow-none hover:drop-shadow-none active:drop-shadow-none active:shadow-none transform ease-in-out duration-500 rounded-sm " + className}
+      className={themeClasses + " w-auto rounded text-white text-sm py-2 px-5 shadow-xl filter drop-shadow-xl hover:shadow-none hover:drop-shadow-none active:drop-shadow-none active:shadow-none transform ease-in-out duration-500 " + className}
     >
       {children}
     </button>
