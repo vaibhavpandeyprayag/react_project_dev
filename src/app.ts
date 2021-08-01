6c63f510533940eaf51f6dd8f1afabfd9b195392
Vaibhav <vaibhavpandeyprayag@gmail.com>
2021-08-01 23:58:24 +0530

    Saving request token in localStorage
    Defining interface LoginResponse, axios.post extends LoginResponse, the request token saved in localStorage of localhost:3000, can be viewed in Inspect=>Application=>Storage=>Local Storage=>http://localhost:3000

--- src/app.ts
+++ src/app.ts
@@ -5,9 +5,28 @@ interface loginData {
   password: string;
 }
 
+interface LoginResponse {
+  data: {
+    is_2fa_enabled: boolean;
+  };
+  token: string;
+  user: User;
+}
+
+interface User {
+  id: number;
+  first_name: string;
+  middle_name: string;
+  last_name: string;
+  role: "staff" | "admin";
+}
+
 const BASE_URL = "https://api-dev.domecompass.com";
 
 export const login = (data: loginData) => {
   const url = BASE_URL + "/login";
-  axios.post(url, data).then((response) => console.log(response));
+  axios.post<LoginResponse>(url, data).then((response) => {
+    console.log(response.data.token);
+    localStorage.setItem("login_token", response.data.token);
+  });
 };
