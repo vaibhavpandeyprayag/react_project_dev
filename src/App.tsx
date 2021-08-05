9db9d35ef6a869532f81df3351b163c4c249b969
Vaibhav <vaibhavpandeyprayag@gmail.com>
2021-08-05 19:51:49 +0530

    React-icons used in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { FC, Suspense, useEffect } from "react";
+import { AiOutlineLoading } from "react-icons/ai";
 import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
 import { me } from "./api/auth";
@@ -8,7 +9,6 @@ import AppContainerPageLazy from "./pages/AppContainer/AppContainer.lazy";
 import AuthPageLazy from "./pages/Auth/Auth.lazy";
 import NotFoundPage from "./pages/NotFound.page";
 import { AppState } from "./store";
-import { AiOutlineLoading } from "react-icons/ai";
 
 interface Props {}
 
@@ -24,38 +24,12 @@ const App: FC<Props> = (props) => {
   }, []);
 
   if (!user && token) {
-    return <div> {AiOutlineLoading} </div>;
+    return <div>{AiOutlineLoading}</div>;
   }
 
   return (
     <>
-      <Suspense
-        fallback={
-          <div className="text-blue-800 text-lg">
-            <svg
-              className="w-8 h-8"
-              viewBox="0 0 100 100"
-              enableBackground="new 0 0 0 0"
-            >
-              <path
-                stroke="blue"
-                fill="blue"
-                d="M73,50c0-12.7-10.3-23-23-23S27,37.3,27,50 M30.9,50c0-10.5,8.5-19.1,19.1-19.1S69.1,39.5,69.1,50"
-              >
-                <animateTransform
-                  attributeName="transform"
-                  attributeType="XML"
-                  type="rotate"
-                  dur="1s"
-                  from="0 50 50"
-                  to="360 50 50"
-                  repeatCount="indefinite"
-                />
-              </path>
-            </svg>
-          </div>
-        }
-      >
+      <Suspense fallback={<div>{AiOutlineLoading}</div>}>
         <BrowserRouter>
           <Switch>
             <Route path="/" exact>
